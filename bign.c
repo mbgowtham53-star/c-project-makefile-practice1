9d4645eddec2311bed6df2f5e5114ba6387fadf9
Ubuntu <ubuntu@ip-172-31-43-214.ec2.internal>
2025-08-18 17:28:56 +0000

    added bign

---
+++ bign.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Declare variables
+    int n;          // To store the number of elements
+    float big;      // To store the largest number found
+
+    // Prompt the user for the number of elements
+    printf("Enter the number of elements you wish to find the greatest element of: ");
+    scanf("%d", &n);
+
+    // Check if the input is valid
+    if (n <= 0) {
+        printf("Please enter a positive number of elements.\n");
+        return 1;  // Exit with error code
+    }
